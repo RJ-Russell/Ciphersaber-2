88804c2c38f30ff57032bc89cad2746967e3c09e
chupacabra <ryan.russell001@yahoo.com>
2015-11-19 19:34:57 -0800

    removed unneeded comments

--- tau_server.py
+++ tau_server.py
@@ -1,7 +1,7 @@
 import socket
 
 
-HOST = '127.0.0.1'
+HOST = 'localhost'
 PORT = 6283
 BUFFER_SIZE = 1024
 
@@ -21,32 +21,3 @@ while True:
         break
     conn.sendall(response.encode())
 conn.close()
-#	socket.gethostbyname(sent_to) # dns ip resolution
-	
-#	s = socket.socket(socket.AF_INET.SOCK_STREAM)
-#	s.bind((HOST,PORT))
-#	s.listen(1)
-
-#	conn, addr = s.accept()
-
-#	print ("Connected by: ", addr)
-
-#	while 1:
- #		data = conn.recv(1024)
-  #		if not data: break
-   #	conn.sendall(data)
-
-	#conn.close()
-
-#	TCP_IP = '127.0.0.1'
-#	7 TCP_PORT = 5005
- #	8 BUFFER_SIZE = 1024
- #	9 MESSAGE = "Hello, World!"
- # 10 
- # 11 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- # 12 s.connect((TCP_IP, TCP_PORT))
- # 13 s.send(MESSAGE)
- # 14 data = s.recv(BUFFER_SIZE)
- # 15 s.close()
- # 16 
- # 17 print "received data:", data
