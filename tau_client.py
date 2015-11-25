59390e51e39f3f9f779d0a3446348f7a18ba0c96
chupacabra <ryan.russell001@yahoo.com>
2015-11-25 02:47:09 -0800

    working on client and server

--- tau_client.py
+++ tau_client.py
@@ -10,113 +10,28 @@ import threading
 
 class Client:
 	host = None
+	port = None
+	buffer_size = None
 	
-
-HOST = ''
-PORT = 6283
-
-#SPORT = 6284
-
-BUFFER_SIZE = 1024
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST,PORT))
-print("Connected to " + (HOST) + "on Port: " + str(PORT))
-connected_message = input("Send: ")
-sock.sendall(connected_message.encode())
-
-while True:
-    data = sock.recv(BUFFER_SIZE).decode()
-    print("Received: " + (data))
-    response = input("Reply: ")
-    if response == "exit":
-        break
-    sock.sendall(response.encode())
-sock.close()
+	def __init__(self, host='localhost', port=6283, buffer_size=1024):
+		self.host = host
+		self.port = port
+		self.buffer_size = buffer_size
 
 
+	def start_client(self):
+		while True:
+			sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			sock_client.connect((self.host,self.port))
+			print("Connected to " + (self.host) + "on Port: " + str(self.port))
+			outgoing_message = input("Send: ")
+			sock_client.sendall(outgoing_message.encode())
 
+			if outgoing_message == "exit":
+				break
+			sock_client.sendall(outgoing_message.encode())
+		sock_client.close()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#MSGLEN = 300
-
-
-
-#hosts = chupa-cabra.ddns.net
-#total_sent = 0
-#while totalsent < MSGLEN:
-#    sent = sock.sent(msg[total_sent])
-#    if sent == 0:
-#        raise RunTimeError("Socket Connection Broken")
-#    total_sent = total_sent + sent
-
-
-
-
-#msg = input("Enter the message: ")
-
-
-
-# get dna resoltion
-# client (receive message)
-
-
-
-# possible send from a different high number port
-# client (receiving)
-# include tcp
-
-# listen port 6238
-# on receive: print message
-#
-#
-# server (sends messages)
-# include tcp
-# open connection to client ip & client port
-# send message
-#
-#
-#
-# client side (sending messages)
-# server side (receiving messages)
-# user interface
-# rc4
-#
-# urandom on front of key
-#
-#
-# if can run program and get cyphertext out , should be able to
-# decrypt with the save IV and same key to get message back out
-#
-# watch for more than one 0 in array...only one character can encrypt to itself
+if __name__ == "__main__":
+	a_client = Client()
+	a_client.start_client()
--- tau_server.py
+++ tau_server.py
@@ -5,29 +5,33 @@ class Server:
 	port = None
 	buffer_size = None
 
-	def __init__(self, ):
-		
+	def __init__(self):
+		self.host = 'localhost'
+		self.port = 6283
+		self.buffer_size = 1024
+	
+	def start_server(self):
+		serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		serv_sock.bind((self.host,self.port))
+		serv_sock.listen(1)
 
-	def start_server():
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.bind((HOST,PORT))
-		s.listen(1)
-
-		conn, addr = s.accept()
+		conn, addr = serv_sock.accept()
 		print("CONNECTION FROM:",addr)
 
 		while True:
-			data = conn.recv(BUFFER_SIZE).decode()
+			data = conn.recv(self.buffer_size).decode()
 			if not data: break
 			print("Received: " + (data))
-			response = input("Reply: ")
-			if response == "exit":
-				break
-			conn.sendall(response.encode())
+		
 		conn.close()
+		return
+		
 
 
 if __name__ == "__main__":
 	a_server = Server()
+	a_server.start_server()
+	
+
 
 
