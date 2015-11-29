81b4689de1dd3fcd9e8cb340a5db530ba710b0af
chupacabra <ryan.russell001@yahoo.com>
2015-11-29 00:05:23 -0800

    took out old code...added some other people to the test list

--- rc4.py
+++ rc4.py
@@ -12,114 +12,111 @@ import random, sys, string
 
 # Key scheduling
 def rc4(input, key, rounds=20):
-	
-	state = range(256)
-	
-	len_state = len(state)
-	len_key = len(key)
-	
-	j = 0
-	for num_round in range(rounds):
-		for i in range(len_state):
-			j = (j + state[i] + key[i % len_key]) % 256
-			state[i],state[j] = state[j], state[i]
-	
-	alt_message = []
-	i = 0
-	j = 0
-	for byte in input:
-		i = (i + 1) % 256
-		j = (j + state[i]) % 256 
-		state[i], state[j] = state[j], state[i]
-		xor_bytes = (state[i] + state[j]) % 256
-		alt_message.append(byte ^ state[xor_bytes])
+    state = range(256)
 
-	return alt_message
+    len_state = len(state)
+    len_key = len(key)
+
+    j = 0
+    for num_round in range(rounds):
+        for i in range(len_state):
+            j = (j + state[i] + key[i % len_key]) % 256
+            state[i], state[j] = state[j], state[i]
+
+    alt_message = []
+    i = 0
+    j = 0
+    for byte in input:
+        i = (i + 1) % 256
+        j = (j + state[i]) % 256
+        state[i], state[j] = state[j], state[i]
+        xor_bytes = (state[i] + state[j]) % 256
+        alt_message.append(byte ^ state[xor_bytes])
+
+    return alt_message
 
 
 def encrypt(plain_message, key, iv=""):
-	print "ENCRYPTING..."
+    print "ENCRYPTING..."
 
-	iv_rand = random.SystemRandom()
-	while len(iv) < 10:
-		iv = iv + chr(int(iv_rand.random()))
-	bytes = rc4(map(ord, plain_message), map(ord, key+iv))
-	return iv + string.join(map(chr, bytes), "")
-	
+    iv_rand = random.SystemRandom()
+    while len(iv) < 10:
+        iv = iv + chr(int(iv_rand.random()))
+    bytes = rc4(map(ord, plain_message), map(ord, key + iv))
+    return iv + string.join(map(chr, bytes), "")
+
 
 def decrypt(cipher_mess, key):
-	print "DECRYPTING..."
+    print "DECRYPTING..."
 
-	iv = cipher_mess[0:10]
-	cipher_mess = cipher_mess[10:]
-	bytes = rc4(map(ord, cipher_mess), map(ord, key+iv))
-	return string.join(map(chr, bytes), "")
-	
+    iv = cipher_mess[0:10]
+    cipher_mess = cipher_mess[10:]
+    bytes = rc4(map(ord, cipher_mess), map(ord, key + iv))
+    return string.join(map(chr, bytes), "")
+
 
 if __name__ == "__main__":
-	
-	#key = "asdfg"
-	#infile = open("testfiles/cstest.cs2", "r")
-	#e = infile.read()
-	#d = decrypt(e, key)
-	#print "D: ", d, "\n"
-	
-	#key1 = "asdfg"
-	#jfile = open("cstest1.cs1", "r")
-	#f = jfile.read()
-	#g = decrypt(f, key1)
-	#print "D: ", g
+    # key = "asdfg"
+    # infile = open("testfiles/cstest.cs2", "r")
+    # e = infile.read()
+    # d = decrypt(e, key)
+    # print "D: ", d, "\n"
 
-	key2 = "SecretMessageforCongress"
-	#ifile = open("testfiles/cstest2.cs1", "r")
-	#i = ifile.read()
-	#print "BEFORE DECRYPT: ", i
+    # key1 = "asdfg"
+    # jfile = open("cstest1.cs1", "r")
+    # f = jfile.read()
+    # g = decrypt(f, key1)
+    # print "D: ", g
 
-	#j = decrypt(i, key2)
-	#print "AFTER DECRYPT: ", j
-	
-	
-	#e = encrypt(message, key)
-	#print "E: ", e
-	
-	f = open("testfiles/test.cs1", "w")
-	message = raw_input("Enter your message: ")
-	z = encrypt(message, key2)
-	f.write(z)
-	f.close()
+    key2 = "SecretMessageforCongress"
+    # ifile = open("testfiles/cstest2.cs1", "r")
+    # i = ifile.read()
+    # print "BEFORE DECRYPT: ", i
 
-	l = open("testfiles/test.cs1", "r")
-	message = l.read()
-	m = decrypt(message, key2)
-	print "Message: ", m, "\n"
-	l.close()
+    # j = decrypt(i, key2)
+    # print "AFTER DECRYPT: ", j
 
-	
-	#filename = sys.argv[1]
-	#a_file = open(filename, "r")
-	#encrypted = a_file.readline().rstrip()
-	#decrypted = decrypt(encrypted,key)
 
-	#f = d.decode("ASCII")
-	#print("F: ", f)
-	
-	#m = rc4(message, key)
-	#print("M: ", m)
-	#c = key_scheduling(key)
-	#print("C: ", c, '\n')
-	#d = key_scheduling(key)
-	#print("D: ", d, '\n')
-	#e = key_scheduling(key)
-	#print("E: ", e, '\n')
-	#f = key_scheduling(key)
-	#print("F: ", f, '\n')
+    # e = encrypt(message, key)
+    # print "E: ", e
 
-	#z = generate_stream(c,message,key)
-	#print("Z: ",z,'\n')
+    f = open("testfiles/test.cs1", "w")
+    message = raw_input("Enter your message: ")
+    z = encrypt(message, key2)
+    f.write(z)
+    f.close()
 
-	#enc_mess = encrypt(message, key)
-	#print("ENCRYPTED MESSAGE: ", enc_mess)
+    l = open("testfiles/test.cs1", "r")
+    message = l.read()
+    m = decrypt(message, key2)
+    print "Message: ", m, "\n"
+    l.close()
 
-	#dec_message = decrypt(enc_mess, key)
-	#print("DECRYPTED MESSAGE: ", dec_message)
 
+# filename = sys.argv[1]
+# a_file = open(filename, "r")
+# encrypted = a_file.readline().rstrip()
+# decrypted = decrypt(encrypted,key)
+
+# f = d.decode("ASCII")
+# print("F: ", f)
+
+# m = rc4(message, key)
+# print("M: ", m)
+# c = key_scheduling(key)
+# print("C: ", c, '\n')
+# d = key_scheduling(key)
+# print("D: ", d, '\n')
+# e = key_scheduling(key)
+# print("E: ", e, '\n')
+# f = key_scheduling(key)
+# print("F: ", f, '\n')
+
+# z = generate_stream(c,message,key)
+# print("Z: ",z,'\n')
+
+# enc_mess = encrypt(message, key)
+# print("ENCRYPTED MESSAGE: ", enc_mess)
+
+# dec_message = decrypt(enc_mess, key)
+# print("DECRYPTED MESSAGE: ", dec_message)
--- tau_client.py
+++ tau_client.py
@@ -23,17 +23,15 @@ class TauClient:
     def __init__(self, host, key='asdfg'):
         self.host = host
         self.key = key
+        print self.key
 
     def connect_client(self):
         while True:
-            print
-            "Creating socket..."
+            print "Creating socket..."
             client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            print
-            "Connecting to host and port..."
+            print "Connecting to host and port..."
             client_sock.connect((self.host, PORT))
-            print
-            "Connected..."
+            print "Connected..."
             self.send_message(client_sock)
 
     def send_message(self, client_sock):
@@ -44,10 +42,9 @@ class TauClient:
 
         encrypt_mess = rc4.encrypt(message, self.key)
         client_sock.send(encrypt_mess)
-        print
-        "Sending message..."
-        print
-        "Sent encrypted message: ", encrypt_mess
+        # print client_sock.send(encrypt_mess)
+        print "Sending message..."
+        print "Sent encrypted message: ", encrypt_mess
         client_sock.shutdown(socket.SHUT_RDWR)
         client_sock.close()
 
@@ -58,6 +55,7 @@ class TauClient:
 
 
 if __name__ == '__main__':
-    client = TauClient('pi.arenjae.com')
-    # client = TauClient('chupa-cabra.ddns.net')
+    client = TauClient('pi.arenjae.com', "password")
+    #client = TauClient('chupa-cabra.ddns.net')
+    #client = TauClient('minion.mindtax.net','abcdf')
     client.connect_client()
