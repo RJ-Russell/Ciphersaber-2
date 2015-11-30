2e6773cfc26363f1604475b20249b3ca3bddb5b9
chupacabra <ryan.russell001@yahoo.com>
2015-11-29 23:36:29 -0800

    added interface class, and loaded json address table

--- tau_interface.py
+++ tau_interface.py
@@ -9,14 +9,22 @@ Andrew Wood:
 References: Daniel Zappala..BYU Python Tutorial.http://ilab.cs.byu.edu/python/
             Python Docs:........................https://www.python.org/
 """
-import tau_server
-import tau_client
+import json
+from tau_client import TauClient
 
 class TauClientInterface:
+    address_book = json.load(open("tau_table.json"))
+    version = "Version: 0.2\r\n"
+    sender = "chupacabra"
+    receiver = None
+
+    def pick_user(self):
+        pick_receiver = input("Enter a name to send message to: ")
+        
 
 
     def main():
-        client = tau_client.TauClient
+
 
 
 
