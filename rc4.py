3f5b048e072a10d496e3cb0245af9cb4a08b3381
chupacabra <ryan.russell001@yahoo.com>
2015-12-02 15:09:55 -0800

    timestamp

--- rc4.py
+++ rc4.py
@@ -63,7 +63,7 @@ def decrypt(cipher_mess, key):
     iv = cipher_mess[0:10]
     cipher_mess = cipher_mess[10:]
     bytes = rc4(map(ord, cipher_mess), map(ord, key + iv))
-    return string.join(map(chr, bytes), "") + "Received on: " + get_time() + "\n"
+    return string.join(map(chr, bytes), "") + "\nReceived on: " + get_time() + "\n"
 
 
 if __name__ == "__main__":
