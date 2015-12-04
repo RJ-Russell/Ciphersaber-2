fb3e6f928d6c54fa4643ef112b4fb7a15e36df14
chupacabra <ryan.russell001@yahoo.com>
2015-12-03 18:24:10 -0800

    added address to received ouput

--- tau_server.py
+++ tau_server.py
@@ -42,7 +42,7 @@ class TauServer:
             message = client.recv(BUFFERSIZE)
             if message:
                 dec_mess = rc4.decrypt(message, key='password')
-                print dec_mess
+                print dec_mess + "\n" + address
                 print "(Press Control-C to Exit Server...)\n"
                # client.send(dec_mess)
 
